b2f1f348e4dd7e437557737a2b2e5e016621b81f
rhyuzi <azitom2@gmail.com>
2024-07-20 19:46:42 +0700

    newset

---
+++ js/asset-image.js
@@ -0,0 +1,158 @@
+var jsonData = [
+    {
+        "name": "Meet On Ciwidey",
+        "figcaption": "Best Template Site",
+        "url": "img/gallery/img-01.jpg"
+    },
+    {
+        "name": "Number 2",
+        "figcaption": "Tooplate HTML CSS",
+        "url": "img/gallery/img-02.jpg"
+    },
+    {
+        "name": "Number 3",
+        "figcaption": "Free Template",
+        "url": "img/gallery/img-03.jpg"
+    },
+    {
+        "name": "Number 4",
+        "figcaption": "Free Download",
+        "url": "img/gallery/img-04.jpg"
+    },
+    {
+        "name": "Number 5",
+        "figcaption": "Mobile Ready",
+        "url": "img/gallery/img-05.jpg"
+    },
+    {
+        "name": "Number 6",
+        "figcaption": "Responsive Design",
+        "url": "img/gallery/img-06.jpg"
+    },
+    {
+        "name": "Number 7",
+        "figcaption": "Custom HTML CSS",
+        "url": "img/gallery/img-07.jpg"
+    },
+    {
+        "name": "Number 8",
+        "figcaption": "Template for you",
+        "url": "img/gallery/img-08.jpg"
+    },
+    {
+        "name": "Number 9",
+        "figcaption": "Get it FREE",
+        "url": "img/gallery/img-09.jpg"
+    },
+    {
+        "name": "Number 10",
+        "figcaption": "Tell your friends",
+        "url": "img/gallery/img-10.jpg"
+    },
+    {
+        "name": "Picture 11",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-11.jpg"
+    },
+    {
+        "name": "Item 12",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-12.jpg"
+    },
+    {
+        "name": "Number 13",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-13.jpg"
+    },
+    {
+        "name": "Picture 14",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-14.jpg"
+    },
+    {
+        "name": "Pic 15",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-15.jpg"
+    },
+    {
+        "name": "Pic 16",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-16.jpg"
+    },
+    {
+        "name": "Number 17",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-17.jpg"
+    },
+    {
+        "name": "Pic 18",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-18.jpg"
+    },
+    {
+        "name": "Pic 19",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-19.jpg"
+    },
+    {
+        "name": "Number 20",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-20.jpg"
+    },
+    {
+        "name": "Pic 21",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-21.jpg"
+    },
+    {
+        "name": "Pic 22",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-22.jpg"
+    },
+    {
+        "name": "Pic 23",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-23.jpg"
+    },
+    {
+        "name": "Number 24",
+        "figcaption": "Web Design 2021",
+        "url": "img/gallery/img-24.jpg"
+    },
+    {
+        "name": "Number 25",
+        "figcaption": "Website Design",
+        "url": "img/gallery/img-25.jpg"
+    },
+    {
+        "name": "Number 26",
+        "figcaption": "HTML CSS Layout",
+        "url": "img/gallery/img-05.jpg"
+    },
+    {
+        "name": "Number 27",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-04.jpg"
+    },
+    {
+        "name": "Number 28",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-03.jpg"
+    },
+    {
+        "name": "Number 29",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-02.jpg"
+    },
+    {
+        "name": "Number 30",
+        "figcaption": "Aliquam lacinia",
+        "url": "img/gallery/img-01.jpg"
+    },
+    {
+        "name": "Meet On Ciwidey",
+        "figcaption": "Best Template Site",
+        "url": "../img/cava5.jpeg"
+    }
+]
+
